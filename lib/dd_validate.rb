270a748bdf9161a7745b8bb1cd7c5554167b68f7
patrick brisbin <pbrisbin@gmail.com>
2024-01-22 14:03:30 -0500

    Begin ruby library to validate service.datadog.yaml files

---
+++ lib/dd_validate.rb
@@ -0,0 +1,55 @@
+require "json"
+require "json-schema"
+require "net/http"
+require "yaml"
+
+# Register draft-07 used by DD schemas
+class Draft07 < JSON::Schema::Draft4
+  def initialize
+    super
+
+    @uri = JSON::Util::URI.parse("http://json-schema.org/draft-07/schema#")
+    @names = ["http://json-schema.org/draft-07/schema#"]
+  end
+
+  JSON::Validator.register_validator(self.new)
+end
+
+module DDValidate
+  class FetchDDSchema
+    def self.call(version)
+      uri = URI("https://raw.githubusercontent.com/DataDog/schema/main/service-catalog/#{version}/schema.json")
+      res = Net::HTTP.get_response(uri)
+
+      unless res.is_a?(Net::HTTPSuccess)
+        $stderr.puts "Unable to fetch DataDog schema for service-catalog"
+        $stderr.puts "URI: #{uri}"
+        $stderr.puts "Response: #{res}"
+        $stderr.puts "Your schema-version may be invalid"
+        exit 1
+      end
+
+      res.body
+    end
+  end
+
+  class DDService
+    def self.load(path, schema_fetcher = FetchDDSchema)
+      YAML.load_stream(File.read(path)).map do |definition|
+        self.new(definition, schema_fetcher)
+      end
+    end
+
+    attr_reader :name, :errors
+
+    def initialize(definition, schema_fetcher)
+      schema_version = definition.fetch("schema-version")
+      schema = schema_fetcher.call(schema_version)
+
+      @name = definition["dd-service"] || "unknown"
+      @errors = JSON::Validator.fully_validate(schema, definition)
+    end
+
+    private_class_method :new
+  end
+end
